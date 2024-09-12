b2dbfbab17f017b5552e6fc73dfcd114b9a03c3a
Jan Lelis <hi@syntaxtransformation.com>
2024-09-12 23:49:58 +0200

    Release v1.10.0

--- lib/unicode/scripts/constants.rb
+++ lib/unicode/scripts/constants.rb
@@ -2,7 +2,7 @@
 
 module Unicode
   module Scripts
-    VERSION = "1.9.0"
+    VERSION = "1.10.0"
     UNICODE_VERSION = "16.0.0"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/").freeze
     INDEX_FILENAME = (DATA_DIRECTORY + "/scripts.marshal.gz").freeze
